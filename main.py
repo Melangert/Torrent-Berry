d5628396d0d5c594b414cb50d13dc6a51e8d506e
Melangert <kosikevinonu@gmail.com>
2026-06-14 21:03:04 -0400

    added network ip to main.py so that its shown on startup

--- main.py
+++ main.py
@@ -4,20 +4,33 @@ from config import API_HOST, API_PORT
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
+import socket
 import threading
 from db import init_db
 from downloader import run
 from api.routes import auth, torrents, status
 from fastapi.staticfiles import StaticFiles
 
+def get_local_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(("8.8.8.8", 80))
+        return s.getsockname()[0]
+    finally:
+        s.close()
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     init_db()
+    ip = get_local_ip()
+    print(f"Access the web UI at: http://{ip}:{API_PORT}")
     thread = threading.Thread(target=run, daemon=True)
     thread.start()
     yield
 
 app = FastAPI(title="Torberry", lifespan=lifespan)
+
+
 
 app.add_middleware(
     CORSMiddleware,
